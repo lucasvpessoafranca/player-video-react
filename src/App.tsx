504f96b5e838e5301f535a70c68a687f4329d069
Lucas França <lucasvpessoafranca@gmail.com>
2023-08-06 18:07:28 -0300

    feat: add play, pause, mute, PiP

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,59 @@ import videoSrc from './video.mp4'
 export function App() {
   const video = useRef<HTMLVideoElement>(null)
   const [playing, setPlaying] = useState(false)
+  useEffect(() => console.log("Funcionou"), [])
 
-  useEffect(() => {
 
-  }, [])
+  function forward() {
+    if(video.current) {
+
+      video.current.currentTime = video.current.currentTime +  2
+    }
+  }
+
+  function changePlayBackRate(speed:number) {
+    if(!video.current)return ;
+    video.current.playbackRate =speed
+  }
+
+  function mute () {
+    if(!video.current) return 
+    video.current.muted = !video.current.muted
+  }
+
+ async function pictureInPicture() {
+  if(!video.current) return;
+  if(document.pictureInPictureElement) {
+      await document.exitPictureInPicture()
+  } else {
+    
+    await video.current.requestPictureInPicture()
+  }
+ 
+  }
+
+
   return (
     <div>
-      <p>{playing? 'tocando' : 'nao tocando'}</p>
-      <div>
-        <button onClick={()=> video.current?.play()}>Play</button>
-        <button onClick={()=> video.current?.pause()}>Pause</button>
+      <div className="flex">
 
-        <div>
-          <video controls src={videoSrc} onPlay={()=> setPlaying(true)} onPause={()=> setPlaying(false)}></video>
-        </div>
-
-
-
+       
+        {playing ? <button onClick={() => video.current?.pause()}>Pause</button> : <button onClick={() => video.current?.play()}>Play</button>}
+        <button onClick={()=> forward()}> + 2s </button>
+        <button onClick={()=> changePlayBackRate(1)}>  1x </button>
+        <button onClick={()=> changePlayBackRate(2)}>  2x </button>
+        <button onClick={mute}>M</button>
+        <button onClick={pictureInPicture}>PiP</button>
       </div>
+
+      <video controls src={videoSrc}
+        ref={video}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+
+
+      ></video>
     </div>
   )
+
 }
